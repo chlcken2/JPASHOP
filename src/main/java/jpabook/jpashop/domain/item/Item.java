c3df2f26deb2822d68dacd2ee66f9b7f6f3350b7
chlcken2 <ckdgus988@naver.com>
2022-07-30 13:51:48 +0900

    FIX: 상속매핑 전략 수정

--- src/main/java/jpabook/jpashop/domain/item/Item.java
+++ src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -12,8 +12,8 @@ import java.util.List;
 @Entity
 @Getter@Setter
 @Inheritance(strategy = InheritanceType.SINGLE_TABLE)
-@DiscriminatorColumn(name = "dtype")
-public abstract class Item {
+@DiscriminatorColumn
+public class Item {
 
     @Id@GeneratedValue
     @Column(name = "item_id")
