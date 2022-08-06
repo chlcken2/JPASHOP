9f48b3af0630ad885a56397e49076fbfec89bc20
chlcken2 <ckdgus988@naver.com>
2022-08-06 12:43:26 +0900

    FIX (서비스): JPA 기본편 시청하고와서 기존 데이터 삭제

--- src/main/java/jpabook/jpashop/JpashopApplication.java
+++ src/main/java/jpabook/jpashop/JpashopApplication.java
@@ -1,15 +1,40 @@
 package jpabook.jpashop;
 
+import jpabook.jpashop.domain.Order;
+import jpabook.jpashop.domain.OrderItem;
+import jpabook.jpashop.domain.item.Book;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
 import java.util.jar.JarEntry;
 
 @SpringBootApplication
 public class JpashopApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(JpashopApplication.class, args);
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory();
+		EntityManager em = emf.createEntityManager();
+		EntityTransaction tx = em.getTransaction();
+		tx.begin();
+		try {
+			Book book = new Book();
+			book.setName("JPA");
+			book.setAuthor("김영한");
+
+			em.persist(book);
+			tx.commit();
+		} catch (Exception e) {
+			tx.rollback();
+		} finally {
+			em.close();
+		}
+		emf.close();
+
+//		SpringApplication.run(JpashopApplication.class, args);
 	}
 
 }
--- src/main/java/jpabook/jpashop/domain/Member.java
+++ src/main/java/jpabook/jpashop/domain/Member.java
@@ -1,29 +1,4 @@
 package jpabook.jpashop.domain;
 
-import lombok.Getter;
-import lombok.Setter;
-
-import javax.persistence.*;
-import java.util.ArrayList;
-import java.util.List;
-
-@Entity
-@Getter
-@Setter
 public class Member {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    @Column(name = "member_id")
-    private Long id;
-
-    private String name;
-
-    @Embedded //내장타입을 가지고있다
-
-    private Address address;
-
-    @OneToMany(mappedBy = "member") // order에 있는 member 필드에 의해 매핑된거란 선언
-    private List<Order> orders = new ArrayList<>();
-
 }
