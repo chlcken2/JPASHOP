074ab52a2aa42dd0286a7112d80b2c61a3373acf
chlcken2 <ckdgus988@naver.com>
2022-08-14 23:03:38 +0900

    FIX : 회원 생성 객체 데이터 값 및 순서 수정

--- src/main/java/jpabook/jpashop/controller/MemberController.java
+++ src/main/java/jpabook/jpashop/controller/MemberController.java
@@ -30,7 +30,7 @@ public class MemberController {
         if (result.hasErrors()) {
             return "members/createMemberForm";
         }
-        Address address = new Address(form.getCity(), form.getName(), form.getZipcode());
+        Address address = new Address(form.getCity(),  form.getStreet(), form.getZipcode());
 
         Member member = new Member();
         member.setName(form.getName());
