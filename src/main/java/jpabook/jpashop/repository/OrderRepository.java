9ecc2d39140196e2d39ba36388a425f0a9c4ebe4
chlcken2 <ckdgus988@naver.com>
2022-08-17 22:51:30 +0900

    [API] :Repository가 화면에 의존하고 있는 안좋은 예시

--- src/main/java/jpabook/jpashop/repository/OrderRepository.java
+++ src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -1,5 +1,6 @@
 package jpabook.jpashop.repository;
 
+
 import jpabook.jpashop.domain.Order;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
@@ -84,4 +85,12 @@ public class OrderRepository {
                         " join fetch o.delivery d", Order.class
         ).getResultList();
     };
+
+    public List<OrderSimpleQueryDto> findOrderDtos() {
+        return em.createQuery("select new jpabook.jpashop.repository.OrderSimpleQueryDto(o.id, m.name, o.orderDate, o.status, d.address)" +
+                " from Order o" +
+                " join o.member m" +
+                " join o.delivery d", OrderSimpleQueryDto.class)
+                .getResultList();
+    }
 }
