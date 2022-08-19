f9f72cb377fbf364ae956cbaee203b07aa769db8
chlcken2 <ckdgus988@naver.com>
2022-08-20 00:00:37 +0900

    Revert "[API] : This reverts commit 3bbb9e1e rollBack

--- src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
+++ src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
@@ -43,18 +43,18 @@ public class OrderSimpleApiController {
 
     }
 
-    @GetMapping("/api/v3/simple-orders")
-    public List<SimpleOrderDto> ordersV3() {
-        List<Order> orders = orderRepository.findAllWithMemberDelivery();
-        List<SimpleOrderDto> result = orders.stream()
-                .map(o -> new SimpleOrderDto(o))
-                .collect(Collectors.toList());
-        return result;
-    }
-
+//    @GetMapping("/api/v3/simple-orders")
+//    public List<SimpleOrderDto> ordersV3() {
+//        List<Order> orders = orderRepository.findAllWithMemberDelivery();
+//        List<SimpleOrderDto> result = orders.stream()
+//                .map(o -> new SimpleOrderDto(o))
+//                .collect(Collectors.toList());
+//        return result;
+//    }
+//
 //    @GetMapping("/api/v4/simple-orders")
 //    public List<SimpleOrderDto> ordersV4() {
-//        return orderRepository.findOrderDtos();
+//        return orderSimpleQueryRepository.findOrderDtos();
 //    }
 
     @Data
